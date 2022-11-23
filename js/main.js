fed9c425d1fa9c807190d3d3c599b2e48eea81b8
Nihat <abdullazadenihat@gmail.com>
2022-11-24 03:23:08 +0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+let Sand = {
+   top: 0,
+}
+let sandEl = document.querySelector('.sand');
+
+// let i = 0
+// let top = 0;
+
+
+// setInterval(runSand, 100)
+// function runSand() {
+//    // if (i == sandArr.length) {
+//    //    i = 0;
+//    // }
+//    // const el = sandArr[i];
+//    // sandEl.style.top = el[0] + "px"
+//    // sandEl.style.left = el[1] + "px"
+//    // sandEl.style.width = el[2] + "px"
+//    // sandEl.style.height = el[3] + "px"
+//    // i++;
+//    // console.log("w")
+//    while (top <= 50) {
+//       
+//       top++;
+//    }
+
+// }
+
+// runSand();
+
+sandArr = [
+   // top left width height
+   [0, 60, 180, 80],
+   [1, 60, 180, 80],
+   [2, 60, 180, 80],
+   [3, 60, 180, 80],
+   [4, 60, 180, 80],
+   [5, 60, 180, 80],
+   [6, 60, 180, 80],
+   [7, 60, 180, 80],
+   [8, 60, 180, 80],
+   [9, 60, 180, 80],
+   [10, 60, 180, 80],
+   [11, 60, 180, 80],
+   [6, 60, 180, 80],
+   [6, 60, 180, 80],
+]
+
+
+
